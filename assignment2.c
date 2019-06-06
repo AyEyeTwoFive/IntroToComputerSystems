d2d9326c3532e74e578be73d91bd25e2a002963e
asef <asef@Lenovo-PC.localdomain>
2019-06-05 20:22:15 -0400

    almost done but seems searching for an ID isnt working

--- assignment2.c
+++ assignment2.c
@@ -22,7 +22,6 @@ int main() {
   int numStudents = 0;
   struct student *studs;
   FILE *ptr; 
-  ptr = fopen("students.bin", "wb");
   while(1) {
     print_menu(); 
     int choice; 
@@ -34,6 +33,7 @@ int main() {
       scanf("%d", &num);
       numStudents = num;
       struct student *data = malloc(num * sizeof(struct student));
+      ptr = fopen("students.bin", "wb");
       for (int i = 0; i < num; i++) {
         char token[100];
         printf("Enter Student ID\n");
@@ -55,6 +55,7 @@ int main() {
         printf("******************\n"); 
       }
       studs = data;
+      fclose(ptr);
     }  
     else if (choice == 2) {
       for (int i = 0; i < numStudents; i++) {
@@ -64,6 +65,106 @@ int main() {
         printf("Course ID := %s\n", studs[i].course);
         printf("Grade := %s\n", studs[i].grade);
         printf("*****************************\n");
+      }
+    }
+    else if (choice == 3) {
+      char search_id[100];
+      int found = 0;
+      printf("Enter Student ID to search for\n");
+      scanf("%s", search_id);
+      for (int i=0; i < numStudents; i++) {
+        if (strcmp(search_id, studs[i].grade) == 0) {
+          printf("Student ID := %s\n", studs[i].id);
+          printf("Student Name := %s\n", studs[i].name);
+          printf("Email ID := %s\n", studs[i].email);
+          printf("Course ID := %s\n", studs[i].course);
+          printf("Grade := %s\n", studs[i].grade);
+          printf("*****************************\n");
+          found = 1;
+        }
+      }
+      if (found == 0) {
+        printf("Student ID not found\n");
+      }
+    }
+    else if (choice == 4) {
+      char search_id[100];
+      int found = 0;
+      printf("Enter Student ID to update\n");
+      scanf("%s", search_id);
+      for (int i=0; i < numStudents; i++) {
+        if (strcmp(search_id, studs[i].grade) == 0) {
+          printf("Which field to update?\n");
+          printf("1. Student ID := %s\n", studs[i].id);
+          printf("2. Student Name := %s\n", studs[i].name);
+          printf("3. Email ID := %s\n", studs[i].email);
+          printf("4. Course ID := %s\n", studs[i].course);
+          printf("5. Grade := %s\n", studs[i].grade);
+          printf("*****************************\n");
+          int field;
+          scanf("%d", &field);
+          char updated[100]; 
+          printf("Update to what?\n");
+          scanf("%s", updated); 
+          if (field == 1) {
+            strcpy(studs[i].id, updated);
+          }
+          else if (field == 2) {
+            strcpy(studs[i].name, updated);
+          }
+          else if (field == 3) {
+            strcpy(studs[i].email, updated);
+          }
+          else if (field == 4) {
+            strcpy(studs[i].course, updated);
+          }
+          else if (field == 5) {
+            strcpy(studs[i].grade, updated);
+          }
+          found = 1;
+        }
+      }
+      if (found == 0) {
+        printf("Student ID not found\n");
+      }
+    }
+    else if (choice == 5) {
+      char delete_name[100];
+      int del_ind;
+      int found;
+      printf("What student name to delete?\n");
+      scanf("%s", delete_name);
+      for (int i=0; i < numStudents; i++) {
+        if (strcmp(delete_name, studs[i].name) == 0) {
+          found = 1;
+          del_ind = i;
+        }
+      }
+      if (found == 0) {
+        printf("Student ID not found\n");
+      }
+      else {
+        struct student *data = malloc((numStudents - 1) * sizeof(struct student));
+        ptr = fopen("students.bin", "wb");
+        for (int i = 0; i < del_ind; i++) {
+          strcpy(data[i].id, studs[i].id);
+          strcpy(data[i].name, studs[i].name);
+          strcpy(data[i].email, studs[i].email);
+          strcpy(data[i].course, studs[i].course);
+          strcpy(data[i].grade, studs[i].grade);
+          fwrite(&data[i], sizeof(struct student), 1, ptr);
+        }
+        for (int i = del_ind + 1; i < numStudents; i++) {
+          strcpy(data[i].id, studs[i].id);
+          strcpy(data[i].name, studs[i].name);
+          strcpy(data[i].email, studs[i].email);
+          strcpy(data[i].course, studs[i].course);
+          strcpy(data[i].grade, studs[i].grade);
+          fwrite(&data[i], sizeof(struct student), 1, ptr);
+        }
+        numStudents -= 1; 
+        studs = data;
+        fclose(ptr);
       }
     }
     else if (choice == 6) {
